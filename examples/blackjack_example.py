9cf260e1ba5c7e4fec2c98a185136a86d3ed3429
Simon Lemieux <lemieux.simon@gmail.com>
2013-01-22 22:02:31 -0500

    forgot a detail

--- examples/blackjack_example.py
+++ examples/blackjack_example.py
@@ -6,7 +6,8 @@ logging.basicConfig(level='INFO')
 
 players = [SimplePlayer()]
 dealer = Dealer()
-deck = Deck(symbols = ['A', '10', '8'])
+#deck = Deck(symbols = ['A', '10', '8'])
+deck = Deck()
 table = Table(players, dealer, deck)
 
 for x in xrange(1, 10):
