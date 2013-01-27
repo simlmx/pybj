276c0b40fefa9e640e4c4f8c2592384538d18bdd
Simon Lemieux <lemieux.simon@gmail.com>
2013-01-27 14:26:18 -0500

    not much

--- examples/interactive.py
+++ examples/interactive.py
@@ -4,12 +4,11 @@ from pybj.players import SimplePlayer, InteractivePlayer
 import logging
 logging.basicConfig(level='INFO')
 
-players = [SimplePlayer(), InteractivePlayer()]
+players = [InteractivePlayer()]
 dealer = Dealer()
-#deck = Deck(symbols = ['A', '10', '8'])
 deck = Deck()
 table = Table(players, dealer, deck)
 
-for x in xrange(1, 10):
-    print x
+for x in xrange(1, 1000):
+    print 'Turn {}'.format(x)
     table.turn()
